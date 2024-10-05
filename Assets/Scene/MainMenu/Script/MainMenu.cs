082a9413719942b47e51e9a6fbf48b754c322826
englishing456 <englishing456@gmail.com>
2024-10-05 10:25:53 +1000

    MainMenu
    The play Animation and Return Animation Are done.

--- Assets/Scene/MainMenu/Script/MainMenu.cs
+++ Assets/Scene/MainMenu/Script/MainMenu.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Reference to the Animator component
-    public Animator animator;
-
-    // Function to be triggered when the Play button is clicked
-    public void OnPlayButtonClicked()
-    {
-        animator.SetTrigger("PlayTransition"); // Ensure this matches your animation clip's name
-    }
-
-public void PlayGame()
+    
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
