89e70d51b84145e958b5d1971568417b18dda386
WayneBrysen <q1092727520@gmail.com>
2024-10-10 23:22:09 +1100

    building the mutiplayer now

---
+++ Assets/CreateAndJoinRooms.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Pun;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
+
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("MainGame_PC");
+    }
+}
