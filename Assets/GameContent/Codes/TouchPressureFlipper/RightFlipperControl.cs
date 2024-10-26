4e85a91e9d3b3233a033f5b3c3291646f97b99be
BrysenWayne <q1092727520@gmail.com>
2024-10-26 11:21:43 +1100

    trying

--- Assets/GameContent/Codes/TouchPressureFlipper/RightFlipperControl.cs
+++ Assets/GameContent/Codes/TouchPressureFlipper/RightFlipperControl.cs
@@ -70,7 +70,7 @@ public class RightFlipperControl : MonoBehaviourPun
 
         if (spring.targetPosition != lastPosition)
         {
-            photonView.RPC("SyncFlipper", RpcTarget.All, spring.targetPosition);
+            photonView.RPC("SyncRightFlipper", RpcTarget.All, spring.targetPosition);
 
             Debug.Log(gameObject.name + " Right Flipper: Position: " + spring.targetPosition);
             lastPosition = spring.targetPosition;
@@ -81,7 +81,7 @@ public class RightFlipperControl : MonoBehaviourPun
     }
 
     [PunRPC]
-    void SyncFlipper(float targetPosition)
+    void SyncRightFlipper(float targetPosition)
     {
         Debug.Log(gameObject.name + " received SyncFlipper RPC with position: " + targetPosition);
 
